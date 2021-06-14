2558f601d360618149fefb0517087c04696f020a
Joshua Onyenwere <joshua.onyenwere@create.at>
2021-06-14 13:49:56 +0300

    Added comments.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+// Function returns a sorted deck of cards
 function generateDeckOfCards() {
     const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
+    // Initialise emty deck array.
     const deck = [];
 
     for (let i = 2; i <= 14; i++) {
@@ -14,12 +17,17 @@ function generateDeckOfCards() {
     return deck;
 }
 
+// Function returns a ramdomised (shuffled) deck of cards.
 // Based on Fisher-Yates shuffle
 // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
 function shuffleDeck(deck) {
+    // Create copy of cards.
     const shuffled = [...deck];
 
+    // Shuffle deck
     for (let i = 0; i < deck.length; i++) {
+
+        // Based on deck length, generate random floating number with Math.random. Math.floor returns the largest integer equal to the given number.
         const j = Math.floor(Math.random() * deck.length);
         const tmp = shuffled[i];
         shuffled[i] = shuffled[j];
@@ -29,11 +37,15 @@ function shuffleDeck(deck) {
     return shuffled;
 }
 
+// Function return Queen cards converted from Aces.
 function convertAcesToQueens(deck) {
+    // Create copy of cards.
     const convertedDeck = [...deck];
 
     for (let i = 0; i < convertedDeck.length; i++) {
         if (convertedDeck[i].startsWith("14")) {
+
+            // Replace all Aces with Queens.
             convertedDeck[i] = convertedDeck[i].replace("14", "12");
         }
     }
@@ -41,13 +53,18 @@ function convertAcesToQueens(deck) {
     return convertedDeck;
 }
 
+
+// Function returns cards per value.
 function countCardsPerValue(deck) {
+    // Initialise emty object to store counted values.
     const countedValues = {};
 
+    // Create deck of cards with value of 0.
     for (let i = 2; i <= 14; i++) {
         countedValues[i] = 0;
     }
 
+    // Get first two digits and store value in countedValues obj. 
     for (const card of deck) {
         const value = card.split(" ")[0];
         countedValues[value] += 1;
@@ -56,6 +73,7 @@ function countCardsPerValue(deck) {
     return countedValues;
 }
 
+// Optimised mapped cards preferred over JS switch statement.
 function getValueName(value) {
     const valueNameMap = {
         2: "Two",
